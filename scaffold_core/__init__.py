6ae007b362e7afa3174af0111968844a23440d25
Aethyrex <128336748+Aethyrex@users.noreply.github.com>
2025-07-23 11:21:53 -0400

    Small Fix to Include Small Sources

--- scaffold_core/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file makes the scaffold_core directory a Python package.
-
-# Ensure scaffold_core is recognized as a module
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
